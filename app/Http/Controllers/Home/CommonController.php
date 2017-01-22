a4ae18c2516a7a6372ed9caa4ad86bc4e0d8f788
zy10242879 <zy10242879@gmail.com>
2017-01-22 18:08:37 +0800

    ***View::share()***common控制器中__construct()用法，共享参数传递的方法 视图中要注意：标签和foreach()要连在一起，不然会有间隙

--- app/Http/Controllers/Home/CommonController.php
+++ app/Http/Controllers/Home/CommonController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers\Home;
 
+use App\Http\Model\Navs;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\View;
 
 class CommonController extends Controller
 {
-    //
+  //---------注意：这里构造函数的用法及共享参数的传递--------
+  public function __construct()
+  {
+    $navs = Navs::all();
+    View::share('navs',$navs);//View::是门面　use Illuminate\Support\Facades\View;
+  }
+  //------View::share('navs',$navs);共享参数传递方法-----
 }
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <!----注意：这里两个section()用法-->
 @section('info')
     @show
+    <!----------------------------->
     <link href="{{asset('resources/views/home/css/base.css')}}" rel="stylesheet">
     <link href="{{asset('resources/views/home/css/index.css')}}" rel="stylesheet">
     <link href="{{asset('resources/views/home/css/style.css')}}" rel="stylesheet">
@@ -15,11 +17,16 @@
 <body>
 <header>
     <div id="logo"><a href="/"></a></div>
-    <nav class="topnav" id="topnav"><a href="index.html"><span>首页</span><span class="en">Protal</span></a><a href="about.html"><span>关于我</span><span class="en">About</span></a><a href="newlist.html"><span>慢生活</span><span class="en">Life</span></a><a href="moodlist.html"><span>碎言碎语</span><span class="en">Doing</span></a><a href="share.html"><span>模板分享</span><span class="en">Share</span></a><a href="knowledge.html"><span>学无止境</span><span class="en">Learn</span></a><a href="book.html"><span>留言版</span><span class="en">Gustbook</span></a></nav>
+    <nav class="topnav" id="topnav">
+        <!------------------注意：这里要将标签连在一起，不然会出现间隙-------------->
+        @foreach($navs as $v)<a href="{{$v->nav_url}}"><span>{{$v->nav_name}}</span><span class="en">{{$v->nav_alias}}</span></a>@endforeach
+        <!------------------------------------------------------------------>
     </nav>
 </header>
+<!----注意：这里两个section()用法-->
 @section('content')
     @show
+<!----------------------------->
 <footer>
     <p>Design by zy10242879 <a href="http://www.miitbeian.gov.cn/" target="_blank">http://github.com/zy10242879</a> <a href="/">网站统计</a></p>
 </footer>
