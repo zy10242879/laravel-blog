b98820efd0580b6196f01c0f08209381001d0c3c
zy10242879 <zy10242879@gmail.com>
2017-01-22 21:22:05 +0800

    **前台页面跳转详情页写法**前台首页控制器，读取数据，视图中遍历显示

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers\Home;
 
 
+use App\Http\Model\Article;
+use App\Http\Model\Links;
+
 class IndexController extends CommonController
 {
   //前台页面首页
   public function index()
   {
-    return view('home.index');
+    //点击量最高的6篇文章
+    $hot = Article::orderBy('art_view','desc')->take(6)->get();
+    //最新发布文章8篇
+    $new = Article::orderBy('art_time','desc')->take(8)->get();
+    //图文列表5篇每页(带分页)
+    $list = Article::orderBy('art_time','desc')->paginate(4);
+    //友情链接
+    $links = Links::orderBy('link_order','asc')->get();
+    return view('home.index',compact('hot','new','list','links'));
   }
   //前台分类列表页
   public function cate()
